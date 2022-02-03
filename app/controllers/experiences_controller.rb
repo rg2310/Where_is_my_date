820e4a4655277efef63cc5c96e57750db4ddf2ba
enora zine <enora.zine@gmail.com>
2022-02-03 19:26:50 +0000

    creation of the controllers and update of experiences controller

---
+++ app/controllers/experiences_controller.rb
@@ -0,0 +1,53 @@
+class ExperiencesController < ApplicationController
+    before_action :set_experience, only: [:show, :edit, :update, :destroy]
+
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @experiences = Experience.where("name LIKE ?","%#{params[:query]}%")
+      # Preventing SQL Injection and Database error for
+      # unknown characters
+    else
+      @experiences = Experience.all
+    end
+  end
+
+  def new
+    @experience = Experience.new
+  end
+
+  def create
+    @experience = Experience.new(experience_params)
+    if @experience.save
+      redirect_to experiences_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @experience.update(experience_params)
+    redirect_to experience_path(@experience)
+  end
+
+  def destroy
+    @experience.destroy
+    redirect_to experiences_path
+  end
+
+  private
+
+  def set_experience
+    @experience = Experience.find(params[:id])
+  end
+
+  def experience_params
+    params.require(:experience).permit(:name, :content, :picture_url, :user_id)
+  end
+end
