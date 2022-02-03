f32e4a6c26053b5418c8a6b814fce047e4bb75a2
Tania Papamikrou <tania.papamikrou@gmail.com>
2022-02-03 21:18:12 +0000

    Created Booking Controller, needs further updating

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,21 @@
 class BookingsController < ApplicationController
+  def create
+      @booking = Booking.new(experience_params)
+    if @booking.save
+      redirect_to experiences_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  private
+
+  def experience_params
+    params.require(:booking).permit(:experience_id, :user_id, :date)
+  end
+
 end
--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class BookingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get create" do
+    get bookings_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get bookings_new_url
+    assert_response :success
+  end
 end
